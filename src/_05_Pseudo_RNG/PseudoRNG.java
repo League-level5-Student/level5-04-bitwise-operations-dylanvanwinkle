bc2ab59631e044673ca35917215d5a7bae3fcc40
dylanvanwinkle <dylanvanwinkle@gmail.com>
2022-01-16 10:27:10 -0800

    PseaudoRNG

--- src/_05_Pseudo_RNG/PseudoRNG.java
+++ src/_05_Pseudo_RNG/PseudoRNG.java
@@ -10,9 +10,11 @@ public class PseudoRNG {
 	 */
 	
 	//1. Create a member variable of type long called "seed"
-	
+	long seed;
 	//2. Make a constructor that initializes the memeber variable
-	
+	PseudoRNG(long l){
+		seed = l;
+	}
 	//3. Complete the steps inside the xorShift method
 	
 	//4. Complete the steps in the main method
@@ -27,29 +29,58 @@ public class PseudoRNG {
 	long xorShift(){
 		//1. Create a local long called seed and set it equal to the member variable 
 		//   We will only use the local variable for the rest of the method
-		
+		long seed = this.seed;
 		//2. Shift seed to the left 13 bits and save the result
-		
+		long seed2 = seed << 13;
 		//3. xor seed by the result of step 1 and save that result
-		
+		seed2 = seed^seed2;
 		//4. Set seed equal to the result of step 2
-	
+	seed = seed2;
 		//5. Shift seed to the right 7 bits and save the result
-		
+		seed2 = seed >> 7;
 		//6. xor seed by the result of step 4 and save that result
-				
+				seed2 = seed^seed2;
 		//7. Set seed equal to the result of step 5
-		
+		seed = seed2;
 		//8. Shift seed to the left 17 bits and save the result
-		
+		seed2 = seed << 17;
 		//9. xor seed by the result of step 7 and save that result
-				
+				seed2 = seed^seed2;
 		//10. Set seed equal to the result of step 8
-		
+		seed = seed2;
 		//11. change the line below to return seed
-		return 0;
+		return seed;
 		
 		//For fun, see if you can rewrite the method using only 4 lines of code
+	}
+	
+	long PseudoR() {
+		long seed = this.seed;
+		
+		//2. Shift seed to the left 13 bits and save the result
+		long seed2 = seed << 10;
+		//3. xor seed by the result of step 1 and save that result
+		seed2 = seed^seed2;
+		//4. Set seed equal to the result of step 2
+	seed = seed2;
+		//5. Shift seed to the right 7 bits and save the result
+		seed2 = seed >> 5;
+		//6. xor seed by the result of step 4 and save that result
+				seed2 = seed^seed2;
+		//7. Set seed equal to the result of step 5
+		seed = seed2;
+		//8. Shift seed to the left 17 bits and save the result
+		seed2 = seed << 200;
+		//9. xor seed by the result of step 7 and save that result
+				seed2 = seed^seed2;
+		//10. Set seed equal to the result of step 8
+				seed = seed2;
+				
+				seed2 = seed >> 100;
+		seed2 = seed^seed2;
+		seed = seed2;
+		return seed;
+		
 	}
 	
 	
@@ -57,12 +88,17 @@ public class PseudoRNG {
 	public static void main(String[] args) {
 	
 		//1. Create a PseudoRNG object with any seed. 
-		
+		PseudoRNG p = new PseudoRNG(100);
 		//2. Print the result of xorShift() 3 times and note the random numbers. 
-		
+	System.out.println(p.xorShift());
+	System.out.println(p.xorShift());
+	System.out.println(p.xorShift());
 		//3. Create a secon PSeudoRNG object with a different seed and print the result of xorShift 3 times.
-		
-		//4. Run the tests a second time and notice that 3 random numbers are the same because the seed generates the same sequence of random numbers.
+	PseudoRNG p2 = new PseudoRNG(10);
+	System.out.println(p2.xorShift());
+	System.out.println(p2.xorShift());
+	System.out.println(p2.xorShift());	
+	//4. Run the tests a second time and notice that 3 random numbers are the same because the seed generates the same sequence of random numbers.
 		
 		//If you would like to set the seed with a number that is hard to predict, you can use System.currentTimeMillis().
 	
